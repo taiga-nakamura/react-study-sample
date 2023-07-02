292e9cf74ea0528e39ad94a00d8f271e8a17aa58
taiga-nakamura <57518278+taiga-nakamura@users.noreply.github.com>
2023-07-03 02:03:39 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export default function App() {
+  console.log("App");
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
